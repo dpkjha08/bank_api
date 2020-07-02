1606ff5e2241fd522050a4942a1e5a8be175b10c
dpk_work <deepak@mymrplace.com>
2020-07-03 02:26:02 +0530

    Corrections

--- api1/urls.py
+++ api1/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('branch/',views.branch,name="post"),
-    path('bank/',views.bank,name="post"),
+    path('branch/',views.branch,name="branch"),
+    path('branch_ifsc/<ifsc>',views.branch_ifsc,name="branch_ifsc"),
+    path('bank_name_city/<name>/<city>',views.bank_name_city,name="bank_name_city"),
+    path('bank/',views.bank,name="bank"),
     
 ]
--- api1/views.py
+++ api1/views.py
@@ -11,6 +11,8 @@ def home(request):
     banks = Banks.objects.all()
     return render(request,"home.html",{"banks":banks})
 
+############################## TO GET REST SERVICE USING FORM ##########################
+
 @api_view(['POST'])
 def branch(request):
     try:
@@ -24,9 +26,14 @@ def branch(request):
                 "bank_name":get_bank_name.name,
                 "details":serializer.data
             }
-        return Response(json_data)
     except(Branches.DoesNotExist):
-        return HttpResponse("<h1>INVALID IFSC CODE</h1>")
+        json_data={
+            "ok":False,
+            "ifsc":ifsc,
+            "invalid":"IFSC CODE IS INVALID"
+        }
+        return Response(json_data)
+    
 
 
 @api_view(['POST'])
@@ -54,9 +61,60 @@ def bank(request):
             "address":i.address,
         }
         json_data["details"].append(each_details)
+    if len(json_data["details"])==0:
+        json_data["details"]="Given Bank is not in this city"
         
     return Response(json_data)
 
 
 
+############################## TO GET REST SERVICE USING DIRECT LINK ##########################
 
+@api_view(['GET'])
+def branch_ifsc(request,ifsc):
+    try:
+        branch = Branches.objects.get(ifsc=ifsc)
+        get_bank_name = Banks.objects.get(id=branch.bank_id)
+        serializer = BranchesSerializer(branch,many=False)
+        json_data={
+                "ok":True,
+                "ifsc":ifsc,
+                "bank_name":get_bank_name.name,
+                "details":serializer.data
+            }
+    except(Branches.DoesNotExist):
+        json_data={
+            "ok":False,
+            "ifsc":ifsc,
+            "invalid":"IFSC CODE IS INVALID"
+        }
+    return Response(json_data)
+
+
+@api_view(['GET'])
+def bank_name_city(request,name,city):
+    name = name.replace("_"," ").upper()
+    city = city.upper()
+    try:
+        bank = Banks.objects.get(name=name)
+    except(Banks.DoesNotExist):
+        return HttpResponse("<h1>INVALID BANK NAME</h1>")
+
+    details = Branches.objects.filter(city=city,bank_id=bank.id)
+    json_data ={
+        "ok":True,
+        "bank_id":bank.id,
+        "bank_name":bank.name,
+        "city":city,
+        "details":[],
+    }
+    for i in details:
+        each_details={
+            "branch":i.branch,
+            "district":i.district,
+            "state":i.state,
+            "address":i.address,
+        }
+        json_data["details"].append(each_details)
+        
+    return Response(json_data)
